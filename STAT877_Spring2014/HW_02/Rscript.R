832e52096bedabc5a892e802f2a2301b0b7021cf
Duy Nguyen <dnguyen@stat.wisc.edu>
2014-03-12 14:31:19 -0500

    Start working on Question 1

--- STAT877_Spring2014/HW_02/Rscript.R
+++ STAT877_Spring2014/HW_02/Rscript.R
@@ -1,18 +1,30 @@
 rm(list=ls(all.names=TRUE))
 rm(list=objects(all.names=TRUE))
 #dev.off()
-
-#### This is HW? ####
+library(ape)
+#### This is STAT877 HW_02 ####
 
 #### Initialize Header File ####
-FilePath <- '~/github/STAT850_Spring2014/'
-Filename.Header <- paste('~/RScripts/HeaderFile_HW.R', sep='')
-source(Filename.Header)
-source(paste(FilePath, 'fn_Library.R', sep=''))
+FilePath <- '~/github/STAT877_Spring2014/HW_02'
 ################################
 
 ################ Input data #################
 setwd(FilePath)
-Filename <- 'hw.dat'
-Data <- read.table(file=Filename, header=FALSE)
+source("phylogeny.r")
+Filename <- "HIVSIVgag.nex"
+xlist = read.nexus.data(Filename)
+xbin = as.DNAbin(xlist)
+n12 = table(xlist[[1]],xlist[[2]])
+
+############### Question 1: Maximum Likelihood ############
+
+
+
+
+
+
+
+
+
+
 
