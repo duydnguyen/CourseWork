0c1887ed9b0862cb78c31a9d0c934ff521686049
Duy Nguyen <dnguyen@stat.wisc.edu>
2014-03-13 09:49:29 -0500

    Saved current Duy's version

--- STAT877_Spring2014/HW_02/Rscript.R
+++ STAT877_Spring2014/HW_02/Rscript.R
@@ -23,13 +23,42 @@ Eval.Loglh <- function(p, kappaR, kappaY, t, seq1, seq2){
   Q <- TN93(p=p,kappaR=kappaR,kappaY=kappaY)  
   n12 <- table(seq1,seq2)
   P.t <- matrixExp(Q,t)
+  d = dim(n12)
   log.lh = 0
-  for (i in 1:4) {
-    for (j in 1:4) {
-      log.lh = log.lh + n12[i,j]*( log(p[i]) + log(P.t[i,j]))          
+# No missing values  
+  if ((d[1]==4)&(d[2]==4))
+    for (i in 1:4) {
+      for (j in 1:4) {
+        log.lh = log.lh + n12[i,j]*( log(p[i]) + log(P.t[i,j]))          
+      }
     }
+# Missing values
+if ((d[1]==5)&(d[2]==4))
+{
+  for (j in 1:4)
+  {
+    log.lh = log.lh + n12[1,j]*(log(p[j]))
   }
-  return(log.lh)
+  for (i in 2:5)
+    for (j in 1:4)
+    {
+      log.lh = log.lh + n12[i,j]*(log(p[i-1])+log(P.t[i-1,j]))
+    }
+}
+
+if ((d[1]==4)&(d[2]==5))
+{
+  for (i in 1:4)
+  {
+    log.lh = log.lh + n12[i,1]*(log(p[i]))
+  }
+  for (i in 1:4)
+    for (j in 2:5)
+    {
+      log.lh = log.lh + n12[i,j]*(log(p[i])+log(P.t[i,j-1]))
+    }
+}
+return(log.lh)
 }
 
 #try = Eval.Loglh(c(0.2,0.3,0.4,0.1), 3, 4, 0.03, xlist[[1]], xlist[[2]])
@@ -46,27 +75,64 @@ Eval.pi <- function(seq1, seq2){
 ## BALT86 (HIVstrain) and sm83 (SIV strain) with kappaR=3, kappaY=4
 p = Eval.pi(xlist[[3]], xlist[[1]])
 # time starts backward from 1983?
-time = seq(0.5,50,0.5)
+time = seq(0.01,1,0.001)
 log.lh = c(rep(0, length(time)))
 for (i in 1:length(time)){
     log.lh[i] = Eval.Loglh(p, 3, 4, time[i], xlist[[3]], xlist[[1]])
 }
 plot(log.lh ~ time, xlab="function of distance", ylab="log-likelihood", type="l")
 ####### (b) ######
-## Compute pi for entire data
-seq1 = xlist[[1]]
-seq2 = xlist[[2]]
-seq3 = xlist[[3]]
-seq4 = xlist[[4]]
-p=c(rep(0,4))
-total = length(which(seq1 !='-')) + length(which(seq2 !='-')) +
-          length(which(seq3 !='-')) + length(which(seq4 !='-'))
-p[1] <- ( length(which(seq1 =='a')) + length(which(seq2 =='a')) 
-          + length(which(seq3 =='a')) + length(which(seq4 =='a')) ) / total
-p[2] <- ( length(which(seq1 =='c')) + length(which(seq2 =='c')) 
-          + length(which(seq3 =='c')) + length(which(seq4 =='c')) ) / total
-p[3] <- ( length(which(seq1 =='g')) + length(which(seq2 =='g')) 
-          + length(which(seq3 =='g')) + length(which(seq4 =='g')) ) / total
-p[4] <- ( length(which(seq1 =='t')) + length(which(seq2 =='t')) 
-          + length(which(seq3 =='t')) + length(which(seq4 =='t')) ) / total
+## function to find maximum likelihood distance
+seq1 <- xlist[[1]]
+seq2 <- xlist[[3]]
+Eval.max <- function(seq1, seq2){
+  p = Eval.pi(seq1, seq2)
+  fn.b = function(t){return(Eval.Loglh(p,2,3,t, seq1, seq2))}
+  MLE = optim(0.1,fn.b,control=list(fnscale=-1),method="Brent",lower=0.001,upper=1)
+  return(MLE)
+} 
+
+try <-Eval.max(seq1,seq2)
+
+
+
+
+####### (c) ######
+## function in R that will optimize the likelihood: parameters = time, kappaR, kappaY
+## x[1]=kappaR, x[2]=kappaY, x[3]= time
+Eval.max.global <- function(seq1,seq2){
+  p = Eval.pi(seq1, seq2)
+  fn.c <- function(x){return(Eval.Loglh(p, x[1], x[2], x[3], seq1, seq2 ))}
+  MLE.c <- optim(c(2,6,0.1),fn.c,control=list(fnscale=-1))
+  return(MLE.c)    
+} 
+
+seq1 <- xlist[[1]]
+seq2 <- xlist[[3]]
+try <-Eval.max.global(seq1,seq2)
+############### Question 2: MCMC ############
+kappaR = 2
+kappaY = 3
+seq1 <- xlist[[1]]
+seq2 <- xlist[[3]]
+p = Eval.pi(seq1, seq2)
+# prior
+f.pr <- function(t)
+{
+  return(10*exp(-10*t)*Eval.Loglh(p, kappaR,kappaY,t,seq1,seq2,t))
+} 
+## Store time distance into vector t 
+t= c()
+# number of samples
+N = 100000 
+t[1]=0.2
+for(i in 2:N)
+{
+  tnew =rexp(1,t[i-1])
+  u=runif(1)
+  b = f(tnew)*dexp(t[i-1],tnew)/(f(t[(i-1)])*dexp(tnew,t[i-1]))
+  t[i]=tnew*(u<=b)+t[i-1]*(u>b)
+}
+
+
 
