0df951618ea7528b50dac2b5a34a399223ad12c8
Duy Nguyen <dnguyen@stat.wisc.edu>
2014-03-13 13:37:35 -0500

    Done Question2

--- STAT877_Spring2014/HW_02/Rscript.R
+++ STAT877_Spring2014/HW_02/Rscript.R
@@ -75,50 +75,69 @@ Eval.pi <- function(){
     p[4]=sum(df=="t")/length(df)
     return(p)  
 }
-
 # Find p 
 p = Eval.pi()
-
-
 ## BALT86 (HIVstrain) and sm83 (SIV strain) with kappaR=3, kappaY=4
-p = Eval.pi(xlist[[3]], xlist[[1]])
-# time starts backward from 1983?
+
 time = seq(0.01,1,0.001)
 log.lh = c(rep(0, length(time)))
 for (i in 1:length(time)){
     log.lh[i] = Eval.Loglh(p, 3, 4, time[i], xlist[[3]], xlist[[1]])
 }
-plot(log.lh ~ time, xlab="function of distance", ylab="log-likelihood", type="l")
+#Plot likelihood function for distance d(sm3, BALT86)
+plot(log.lh ~ time,xlab="t",ylab="Likelihood",main="Likelihood function for d(sm83,BALT86)",
+     type='l',cex.lab=1.5, cex.axis=1.5,cex.main=1.5)
+
 ####### (b) ######
-## function to find maximum likelihood distance
-seq1 <- xlist[[1]]
-seq2 <- xlist[[3]]
+## function to find maximum likelihood distance given each pair of sequences
 Eval.max <- function(seq1, seq2){
-  p = Eval.pi(seq1, seq2)
   fn.b = function(t){return(Eval.Loglh(p,2,3,t, seq1, seq2))}
   MLE = optim(0.1,fn.b,control=list(fnscale=-1),method="Brent",lower=0.001,upper=1)
   return(MLE)
 } 
 
-try <-Eval.max(seq1,seq2)
+d12 <- Eval.max(xlist[[1]],xlist[[2]])
+d13 <- Eval.max(xlist[[1]],xlist[[3]])
+d14 <- Eval.max(xlist[[1]],xlist[[4]])
+d23 <- Eval.max(xlist[[2]],xlist[[3]])
+d24 <- Eval.max(xlist[[2]],xlist[[4]])
+d34 <- Eval.max(xlist[[3]],xlist[[4]])
 
-
-
+# To get the maximum likelihood distance, use d12$par
 
 ####### (c) ######
-## function in R that will optimize the likelihood: parameters = time, kappaR, kappaY
-## x[1]=kappaR, x[2]=kappaY, x[3]= time
+## function in R that will optimize the likelihood: parameters = distance, kappaR, kappaY
+## x[1]=kappaR, x[2]=kappaY, x[3]= distance
 Eval.max.global <- function(seq1,seq2){
-  p = Eval.pi(seq1, seq2)
   fn.c <- function(x){return(Eval.Loglh(p, x[1], x[2], x[3], seq1, seq2 ))}
-  MLE.c <- optim(c(2,6,0.1),fn.c,control=list(fnscale=-1))
+  MLE.c <- optim(c(2,2.8,0.1),fn.c,control=list(fnscale=-1))
   return(MLE.c)    
 } 
 
 seq1 <- xlist[[1]]
 seq2 <- xlist[[3]]
-try <-Eval.max.global(seq1,seq2)
+
+d12.global <- Eval.max.global(xlist[[1]],xlist[[2]])
+# MLEs: kappaR, kappaY, distance(time)
+d12.global$par
+
+d13.global <- Eval.max.global(xlist[[1]],xlist[[3]])
+d14.global <- Eval.max.global(xlist[[1]],xlist[[4]])
+d23.global <- Eval.max.global(xlist[[2]],xlist[[3]])
+d24.global <- Eval.max.global(xlist[[2]],xlist[[4]])
+d34.global <- Eval.max.global(xlist[[3]],xlist[[4]])
+
+## Plots of likelihood: I plot d12. Other cases are similar
+MLE <- d12.global$par
+y = c()
+x = seq(0.01,1,0.001)
+for (i in 1:length(x))
+{
+  y[i] = Eval.Loglh(p,MLE[1],MLE[2],x[i],xlist[[1]],xlist[[2]])
+}
+plot(y~x, type='l')
 ############### Question 2: MCMC ############
+## Proposal distribution q =Exp(t)
 kappaR = 2
 kappaY = 3
 seq1 <- xlist[[1]]
@@ -127,20 +146,59 @@ p = Eval.pi(seq1, seq2)
 # prior
 f.pr <- function(t)
 {
-  return(10*exp(-10*t)*Eval.Loglh(p, kappaR,kappaY,t,seq1,seq2,t))
+  return(10*exp(-10*t)*Eval.Loglh(p, kappaR,kappaY,t,seq1,seq2))
 } 
 ## Store time distance into vector t 
-t= c()
-# number of samples
-N = 100000 
-t[1]=0.2
-for(i in 2:N)
-{
-  tnew =rexp(1,t[i-1])
-  u=runif(1)
-  b = f(tnew)*dexp(t[i-1],tnew)/(f(t[(i-1)])*dexp(tnew,t[i-1]))
-  t[i]=tnew*(u<=b)+t[i-1]*(u>b)
+# N = number of samples
+Sim <-function(N){
+  t= c()
+  t[1]=0.2
+  for(i in 2:N)
+    {
+        tnew =rexp(1,t[i-1])
+        u=runif(1)
+        b = f.pr(tnew)*dexp(t[i-1],tnew)/(f.pr(t[(i-1)])*dexp(tnew,t[i-1]))
+        t[i]=tnew*(u<=b)+t[i-1]*(u>b)
+    }
+return(t)
 }
+t <- Sim(100000)
 
+## Plots
 
+plot(t[99001:100000],type='l',main=' Trace plot for MCMC method',ylab="t",cex.lab=1.5,
+     cex.axis=1.5,cex.main=1.5)
+hist(t[99001:100000],main='Posterior distribution',freq=FALSE, xlab = "t",cex.lab=1.5,
+     cex.axis=1.5,cex.main=1.5)
 
+## Proposal distribution q =Gamma(shape=3, scale=t)
+Sim1 <- function(N){
+  t= c()
+  k = 3
+  t[1]=rgamma(1,shape=k,scale=0.5)
+  
+  for(i in 2:N)
+  {
+    tnew =rgamma(1,shape=k,scale=t[i-1])
+    u=runif(1)
+    b = f.pr(tnew)*dgamma(t[i-1],shape=k,scale=tnew)/(f.pr(t[(i-1)])*dgamma(tnew,shape=k,scale=t[i-1]))
+    t[i]=tnew*(u<=b)+t[i-1]*(u>b)
+  }
+  return(t)
+}
+t <- Sim1(100000)
+
+## Proposal distribution q =Unif(0,1)
+Sim2 <- function(N){
+  t <- c()
+  t[1]=runif(1)
+  for (i in 2:N)
+  {
+    tnew  <- runif(1)
+    u <- runif(1)
+    b = f.pr(tnew)*dunif(t[i-1])/(f.pr(t[(i-1)])*dunif(tnew))
+    t[i]=tnew*(u<=b)+t[i-1]*(u>b)
+  }
+  return(t)
+}
+t <- Sim2(100000)
