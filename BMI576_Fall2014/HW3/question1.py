13c5c3afefde20760628ff38fae8a1e42f1569b2
Duy Nguyen <dnguyen@stat.wisc.edu>
2014-10-29 04:19:39 -0500

    made changes to question1.py

--- BMI576_Fall2014/HW3/question1.py
+++ BMI576_Fall2014/HW3/question1.py
@@ -1,6 +1,6 @@
 #!/user/bin/env python
 # Student: Duy Nguyen
-# Solution to question ?,  homework ?
+# Solution to question 1,  homework 3
 #
 # You should be able to run the program as of the following commands
 
@@ -22,7 +22,38 @@ def read_score(filename):
             row += 1
     return score
 
+def read_tree(filename):
+    "Read child/parent from tree.txt"
+    # Initialize the dictionary to store child/parent relations
+    relations = {}
+    child = 0
+    parent = 0
+    with open(filename, 'r') as file:
+        for line in file:
+            vals = line.strip().split()
+            child = int(vals[0].strip('S'))
+            parent = int(vals[1].strip('S'))
+            relations[child] = parent
+    return relations
+
+def read_assign(filename):
+    "Read assignments at leaf nodes from assign.txt"
+    # Initialize the dictionary for assignments: NO space format s1=a
+    assign = {}
+    leaf = 0
+    character = ''
+    with open(filename, 'r') as file:
+        for line in file:
+            vals = line.strip().split()
+    for i in range(len(vals)):
+        leaf = int(vals[i][1])
+        character = vals[i][3]
+        assign[leaf] = character
+    return assign
+
 if __name__ == '__main__':
     read_score('score.txt')
-#    print(read_score('score.txt'))
+    read_tree('tree.txt')
+    read_assign('assign.txt')
+    print(read_assign('assign.txt'))
 
