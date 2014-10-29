44092385e0d9a4ad762bfa742dd8470d7dd8994e
Duy Nguyen <dnguyen@stat.wisc.edu>
2014-10-29 03:18:45 -0500

    add changes in question1.py

--- BMI576_Fall2014/HW3/question1.py
+++ BMI576_Fall2014/HW3/question1.py
@@ -9,4 +9,20 @@ import sys
 import io
 import getopt
 
-    
+def read_score(filename):
+    "Read score matrix from score.txt"
+    # Initialize 2D list with dim = 4*4
+    score = [[0 for x in range(4)] for y in range(4)]
+    row = 0
+    with open(filename, 'r') as file:
+        file.readline() # omit the first line
+        for line in file:
+            vals = line.strip().split()
+            score[row] = [float(vals[1]), float(vals[2]), float(vals[3]), float(vals[4])]
+            row += 1
+    return score
+
+if __name__ == '__main__':
+    read_score('score.txt')
+#    print(read_score('score.txt'))
+
