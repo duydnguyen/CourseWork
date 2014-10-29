1bc77768b3b8bbfebd8eca847a4e77e2118bb05c
Duy Nguyen <dnguyen@stat.wisc.edu>
2014-10-29 01:39:01 -0500

    add HW3 to BMI576

---
+++ BMI576_Fall2014/HW3/question1.py
@@ -0,0 +1,12 @@
+#!/user/bin/env python
+# Student: Duy Nguyen
+# Solution to question ?,  homework ?
+#
+# You should be able to run the program as of the following commands
+
+
+import sys
+import io
+import getopt
+
+    
